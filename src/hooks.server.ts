18abb5fe34da8d8ee2fe3e8dc78516e79764b494
Henil Malaviya <henilmalaviya06@gmail.com>
2023-12-25 22:59:12 +0530

    implemented log module

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,15 @@
 import { luciaHandler } from '$lib/server/lucia/handler';
+import { LogType, logger } from '$lib/server/modules/log';
 import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle = sequence(luciaHandler);
+export const handle = sequence(luciaHandler, async ({ event, resolve }) => {
+
+    logger()
+        .type(LogType.SUCCESS)
+        .prefix("hook")
+        .prefix(event.request.method.toLowerCase())
+        .message(event.url.pathname)
+        .commit()
+
+    return resolve(event);
+});
